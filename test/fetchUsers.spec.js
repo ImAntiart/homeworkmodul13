ffae10e0eb97db3a605001f7fe79ca70c0460261
Antiart <r2d2rom@mail.ru>
2024-12-06 04:43:57 +0500

    решение проблемы конфиг дж2

--- test/fetchUsers.spec.js
+++ test/fetchUsers.spec.js
@@ -1,19 +1,37 @@
 const fetch = require('node-fetch');
 const sinon = require('sinon');
-const expect = require('chai').expect;
+const chai = require('chai');
+const expect = chai.expect;
+
+// Подключаем  индексный файл
+const fetchUsers = require('./index'); 
 
 describe('fetchUsers', () => {
+    let consoleLogSpy;
+
     beforeEach(() => {
-        sinon.stub(global, 'fetch').resolves({ json: () => Promise.resolve([{ name: 'John Doe' }, { name: 'Jane Smith' }]) });
+        // Создаем шпион на функцию console.log
+        consoleLogSpy = sinon.spy(console, 'log');
+
+        // Создаем заглушку для fetch
+        sinon.stub(global, 'fetch').resolves({
+            json: () => Promise.resolve([
+                { name: 'John Doe' },
+                { name: 'Jane Smith' }
+            ])
+        });
     });
 
     afterEach(() => {
+
         sinon.restore();
     });
 
     it('should log the names of the users', async () => {
         await fetchUsers();
-        expect(console.log).to.have.been.calledWith('John Doe');
-        expect(console.log).to.have.been.calledWith('Jane Smith');
+
+
+        expect(consoleLogSpy.calledWith('John Doe')).to.be.true;
+        expect(consoleLogSpy.calledWith('Jane Smith')).to.be.true;
     });
 });
